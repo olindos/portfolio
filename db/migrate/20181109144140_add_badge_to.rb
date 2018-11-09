002973e9570f1a69c35f613de395d0e061be28a7
olindos <yason4ik87@gmail.com>
2018-11-09 16:57:30 +0200

    Implemented concern for managing image data

---
+++ db/migrate/20181109144140_add_badge_to.rb
@@ -0,0 +1,4 @@
+class AddBadgeTo < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108132724) do
+ActiveRecord::Schema.define(version: 20181109144140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
