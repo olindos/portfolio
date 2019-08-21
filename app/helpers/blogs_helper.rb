559fff55cbb97623b18066817eb29a76e1cc4ae5
olindos <yason4ik87@gmail.com>
2019-08-21 17:45:43 +0300

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -22,4 +22,8 @@ module BlogsHelper
     marcdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     marcdown_to_html.render(text).html_safe
   end
+
+  def blog_status_color blog
+    'color: red;' if blog.draft?
+  end
 end
